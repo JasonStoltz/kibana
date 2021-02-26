343d0061657cf9d5a150e404b184d37396c9d8a7
Jason Stoltzfus <jastoltz24@gmail.com>
2021-02-26 12:12:24 -0500

    How does this look

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/relevance_tuning/relevance_tuning.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/relevance_tuning/relevance_tuning.tsx
@@ -56,135 +56,181 @@ export const RelevanceTuning: React.FC<Props> = ({ engineBreadcrumb }) => {
     initializeRelevanceTuning();
   }, []);
 
+  const pageHeader = () => (
+    <EuiPageHeader
+      className="relevanceTuningHeader"
+      pageTitle={RELEVANCE_TUNING_TITLE}
+      description={i18n.translate(
+        'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.description',
+        {
+          defaultMessage: 'Set field weights and boosts',
+        }
+      )}
+      rightSideItems={
+        engineHasSchemaFields
+          ? [
+              <EuiButton
+                data-test-subj="SaveRelevanceTuning"
+                color="primary"
+                fill
+                onClick={updateSearchSettings}
+              >
+                {i18n.translate(
+                  'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.saveButtonLabel',
+                  {
+                    defaultMessage: 'Save',
+                  }
+                )}
+              </EuiButton>,
+              <EuiButton
+                data-test-subj="ResetRelevanceTuning"
+                color="danger"
+                onClick={resetSearchSettings}
+              >
+                {i18n.translate(
+                  'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.resetButtonLabel',
+                  {
+                    defaultMessage: 'Restore defaults',
+                  }
+                )}
+              </EuiButton>,
+            ]
+          : []
+      }
+    />
+  );
+
+  const invalidBoostsCallout = () => (
+    <EuiCallOut
+      color="warning"
+      iconType="alert"
+      title={i18n.translate(
+        'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.invalidBoostsBannerLabel',
+        {
+          defaultMessage: 'You have invalid boosts!',
+        }
+      )}
+      data-test-subj="RelevanceTuningInvalidBoostsCallout"
+    >
+      {i18n.translate(
+        'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.invalidBoostsErrorMessage',
+        {
+          defaultMessage:
+            'One or more of your boosts is no longer valid, possibly due to a schema type change. Delete any old or invalid boosts to dismiss this alert.',
+        }
+      )}
+    </EuiCallOut>
+  );
+
+  const unsearchedUnconfirmedFieldsCallout = () => (
+    <EuiCallOut
+      color="warning"
+      iconType="alert"
+      title={i18n.translate(
+        'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.uncofirmedFieldsBannerLabel',
+        {
+          defaultMessage: 'Recently added fields are not being searched by default',
+        }
+      )}
+      data-test-subj="RelevanceTuningUnsearchedFieldsCallout"
+    >
+      <FormattedMessage
+        id="xpack.enterpriseSearch.appSearch.engine.relevanceTuning.uncofirmedFieldsErrorMessage"
+        defaultMessage="If these new fields should be searchable, turn them on here by toggling Text Search. Otherwise, confirm your new {schemaLink} to dismiss this alert."
+        values={{
+          schemaLink: (
+            <EuiLinkTo to={generateEnginePath(ENGINE_SCHEMA_PATH)}>
+              {i18n.translate(
+                'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.schemaFieldsLinkLabel',
+                {
+                  defaultMessage: 'schema fields',
+                }
+              )}
+            </EuiLinkTo>
+          ),
+        }}
+      />
+    </EuiCallOut>
+  );
+
+  const schemaFieldsWithConflictsCallout = () => (
+    <EuiCallOut
+      color="warning"
+      iconType="alert"
+      title={i18n.translate(
+        'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.schemaConflictsBannerLabel',
+        {
+          defaultMessage: 'Disabled fields',
+        }
+      )}
+      data-test-subj="SchemaConflictsCallout"
+    >
+      <FormattedMessage
+        id="xpack.enterpriseSearch.appSearch.engine.relevanceTuning.schemaConflictsErrorMessage"
+        defaultMessage="{schemaFieldsWithConflictsCount, number} inactive {schemaFieldsWithConflictsCount, plural, one {field} other {fields}} due to field-type conflicts. {link}"
+        values={{
+          schemaFieldsWithConflictsCount,
+          link: (
+            <EuiLink href={`${DOCS_PREFIX}/meta-engines-guide.html`} target="_blank">
+              {i18n.translate(
+                'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.whatsThisLinkLabel',
+                {
+                  defaultMessage: "What's this?",
+                }
+              )}
+            </EuiLink>
+          ),
+        }}
+      />
+    </EuiCallOut>
+  );
+
+  const emptyCallout = () => (
+    <EuiEmptyPrompt
+      title={
+        <h2>
+          {i18n.translate(
+            'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.emptyErrorMessageTitle',
+            {
+              defaultMessage: 'Tuning requires schema fields',
+            }
+          )}
+        </h2>
+      }
+      body={i18n.translate(
+        'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.emptyErrorMessage',
+        {
+          defaultMessage: 'Index documents to tune relevance.',
+        }
+      )}
+      actions={
+        <EuiButton
+          size="s"
+          color="primary"
+          href={`${DOCS_PREFIX}/relevance-tuning-guide.html`}
+          fill
+        >
+          {i18n.translate(
+            'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.emptyButtonLabel',
+            {
+              defaultMessage: 'Read the relevance tuning guide',
+            }
+          )}
+        </EuiButton>
+      }
+    />
+  );
+
   return (
     <>
       <UnsavedChangesPrompt hasUnsavedChanges={unsavedChanges} />
       <SetPageChrome trail={[...engineBreadcrumb, RELEVANCE_TUNING_TITLE]} />
-      <EuiPageHeader
-        className="relevanceTuningHeader"
-        pageTitle={RELEVANCE_TUNING_TITLE}
-        description={i18n.translate(
-          'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.description',
-          {
-            defaultMessage: 'Set field weights and boosts',
-          }
-        )}
-        rightSideItems={
-          engineHasSchemaFields
-            ? [
-                <EuiButton
-                  data-test-subj="SaveRelevanceTuning"
-                  color="primary"
-                  fill
-                  onClick={updateSearchSettings}
-                >
-                  {i18n.translate(
-                    'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.saveButtonLabel',
-                    {
-                      defaultMessage: 'Save',
-                    }
-                  )}
-                </EuiButton>,
-                <EuiButton
-                  data-test-subj="ResetRelevanceTuning"
-                  color="danger"
-                  onClick={resetSearchSettings}
-                >
-                  {i18n.translate(
-                    'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.resetButtonLabel',
-                    {
-                      defaultMessage: 'Restore defaults',
-                    }
-                  )}
-                </EuiButton>,
-              ]
-            : []
-        }
-      />
+      {pageHeader()}
 
       <FlashMessages />
-      {invalidBoosts && (
-        <EuiCallOut
-          color="warning"
-          iconType="alert"
-          title={i18n.translate(
-            'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.invalidBoostsBannerLabel',
-            {
-              defaultMessage: 'You have invalid boosts!',
-            }
-          )}
-          data-test-subj="RelevanceTuningInvalidBoostsCallout"
-        >
-          {i18n.translate(
-            'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.invalidBoostsErrorMessage',
-            {
-              defaultMessage:
-                'One or more of your boosts is no longer valid, possibly due to a schema type change. Delete any old or invalid boosts to dismiss this alert.',
-            }
-          )}
-        </EuiCallOut>
-      )}
-      {unsearchedUnconfirmedFields && (
-        <EuiCallOut
-          color="warning"
-          iconType="alert"
-          title={i18n.translate(
-            'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.uncofirmedFieldsBannerLabel',
-            {
-              defaultMessage: 'Recently added fields are not being searched by default',
-            }
-          )}
-          data-test-subj="RelevanceTuningUnsearchedFieldsCallout"
-        >
-          <FormattedMessage
-            id="xpack.enterpriseSearch.appSearch.engine.relevanceTuning.uncofirmedFieldsErrorMessage"
-            defaultMessage="If these new fields should be searchable, turn them on here by toggling Text Search. Otherwise, confirm your new {schemaLink} to dismiss this alert."
-            values={{
-              schemaLink: (
-                <EuiLinkTo to={generateEnginePath(ENGINE_SCHEMA_PATH)}>
-                  {i18n.translate(
-                    'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.schemaFieldsLinkLabel',
-                    {
-                      defaultMessage: 'schema fields',
-                    }
-                  )}
-                </EuiLinkTo>
-              ),
-            }}
-          />
-        </EuiCallOut>
-      )}
-      {schemaFieldsWithConflictsCount > 0 && (
-        <EuiCallOut
-          color="warning"
-          iconType="alert"
-          title={i18n.translate(
-            'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.schemaConflictsBannerLabel',
-            {
-              defaultMessage: 'Disabled fields',
-            }
-          )}
-          data-test-subj="SchemaConflictsCallout"
-        >
-          <FormattedMessage
-            id="xpack.enterpriseSearch.appSearch.engine.relevanceTuning.schemaConflictsErrorMessage"
-            defaultMessage="{schemaFieldsWithConflictsCount, number} inactive {schemaFieldsWithConflictsCount, plural, one {field} other {fields}} due to field-type conflicts. {link}"
-            values={{
-              schemaFieldsWithConflictsCount,
-              link: (
-                <EuiLink href={`${DOCS_PREFIX}/meta-engines-guide.html`} target="_blank">
-                  {i18n.translate(
-                    'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.whatsThisLinkLabel',
-                    {
-                      defaultMessage: "What's this?",
-                    }
-                  )}
-                </EuiLink>
-              ),
-            }}
-          />
-        </EuiCallOut>
-      )}
+      {invalidBoosts && invalidBoostsCallout()}
+      {unsearchedUnconfirmedFields && unsearchedUnconfirmedFieldsCallout()}
+      {schemaFieldsWithConflictsCount > 0 && schemaFieldsWithConflictsCallout()}
       <EuiSpacer />
       {engineHasSchemaFields ? (
         <EuiFlexGroup>
@@ -194,39 +240,7 @@ export const RelevanceTuning: React.FC<Props> = ({ engineBreadcrumb }) => {
           <EuiFlexItem />
         </EuiFlexGroup>
       ) : (
-        <EuiEmptyPrompt
-          title={
-            <h2>
-              {i18n.translate(
-                'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.emptyErrorMessageTitle',
-                {
-                  defaultMessage: 'Tuning requires schema fields',
-                }
-              )}
-            </h2>
-          }
-          body={i18n.translate(
-            'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.emptyErrorMessage',
-            {
-              defaultMessage: 'Index documents to tune relevance.',
-            }
-          )}
-          actions={
-            <EuiButton
-              size="s"
-              color="primary"
-              href={`${DOCS_PREFIX}/relevance-tuning-guide.html`}
-              fill
-            >
-              {i18n.translate(
-                'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.emptyButtonLabel',
-                {
-                  defaultMessage: 'Read the relevance tuning guide',
-                }
-              )}
-            </EuiButton>
-          }
-        />
+        emptyCallout()
       )}
     </>
   );
