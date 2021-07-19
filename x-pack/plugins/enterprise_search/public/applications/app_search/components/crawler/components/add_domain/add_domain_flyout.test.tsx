bbb91ee40bfcfb726e2a06602aca7226d02658a7
Jason Stoltzfus <jastoltz24@gmail.com>
2021-07-19 13:27:45 -0400

    change

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/crawler/components/add_domain/add_domain_flyout.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/crawler/components/add_domain/add_domain_flyout.test.tsx
@@ -5,15 +5,13 @@
  * 2.0.
  */
 
-import { mockSetState, mockUseState } from '../../../../../__mocks__/shallow_usestate.mock';
+// import { mockSetState, mockUseState } from '../../../../../__mocks__/usestate.mock.ts';
 
 import React from 'react';
 
 import { shallow, ShallowWrapper } from 'enzyme';
 
 import { EuiButton, EuiButtonEmpty, EuiFlyout, EuiFlyoutBody } from '@elastic/eui';
-
-import { rerender } from '../../../../../test_helpers';
 
 import { AddDomainFlyout } from './add_domain_flyout';
 import { AddDomainForm } from './add_domain_form';
@@ -28,20 +26,13 @@ describe('AddDomainFlyout', () => {
   it('is hidden by default', () => {
     const wrapper = shallow(<AddDomainFlyout />);
 
-    expect(mockUseState).toHaveBeenCalledWith(false);
     expect(wrapper.find(EuiFlyout)).toHaveLength(0);
   });
 
   it('displays the flyout when the button is pressed', () => {
-    mockUseState.mockReturnValueOnce([false, mockSetState]);
     const wrapper = shallow(<AddDomainFlyout />);
 
     wrapper.find(EuiButton).simulate('click');
-
-    expect(mockSetState).toHaveBeenCalledWith(true);
-
-    mockUseState.mockReturnValueOnce([true, mockSetState]);
-    rerender(wrapper);
 
     expect(wrapper.find(EuiFlyout)).toHaveLength(1);
   });
@@ -50,8 +41,8 @@ describe('AddDomainFlyout', () => {
     let wrapper: ShallowWrapper;
 
     beforeEach(() => {
-      mockUseState.mockReturnValueOnce([true, mockSetState]);
       wrapper = shallow(<AddDomainFlyout />);
+      wrapper.find(EuiButton).simulate('click');
     });
 
     it('displays form errors', () => {
@@ -64,7 +55,7 @@ describe('AddDomainFlyout', () => {
 
     it('contains a cancel buttonn', () => {
       wrapper.find(EuiButtonEmpty).simulate('click');
-      expect(mockSetState).toHaveBeenCalledWith(false);
+      expect(wrapper.find(EuiFlyout)).toHaveLength(0);
     });
 
     it('contains a submit button', () => {
@@ -73,11 +64,6 @@ describe('AddDomainFlyout', () => {
 
     it('hides the flyout on close', () => {
       wrapper.find(EuiFlyout).simulate('close');
-
-      expect(mockSetState).toHaveBeenCalledWith(false);
-
-      mockUseState.mockReturnValueOnce([false, mockSetState]);
-      rerender(wrapper);
 
       expect(wrapper.find(EuiFlyout)).toHaveLength(0);
     });
